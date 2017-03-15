6826946f564cb3bc295625b4cba6b97378c1e378
erpi <erpi@users.noreply.github.com>
2017-03-15 18:05:43 +0100

    keuzemogelijkheid 'varia' bij invoeren verslag

--- _scripts/components/verslag-schrijven.js
+++ _scripts/components/verslag-schrijven.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
   $("#kk-info").css('display', ($('#type-verslag').attr("value") == 'kk') ? 'block' : 'none');
   $("#help-tag-ni").css('display', ($('#type-verslag').attr("value") == 'ni') ? 'block' : 'none');
   $("#help-tag-kk").css('display', ($('#type-verslag').attr("value") == 'kk') ? 'block' : 'none');
+  $("#help-tag-varia").css('display', ($('#type-verslag').attr("value") == 'varia') ? 'block' : 'none');
   $('#type-verslag').on('change', function () {
     $("#interclubs-info").css('display', (this.value == 'ni') ? 'block' : 'none');
     $("#kk-info").css('display', (this.value == 'kk') ? 'block' : 'none');
     $("#help-tag-ni").css('display', (this.value == 'ni') ? 'block' : 'none');
     $("#help-tag-kk").css('display', (this.value == 'kk') ? 'block' : 'none');
+    $("#help-tag-varia").css('display', (this.value == 'varia') ? 'block' : 'none');
   });
   // extra input velden voor stelling
   $("#stelling-toevoegen").change(function () {
