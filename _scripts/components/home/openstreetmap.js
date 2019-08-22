e7f55568a3eb134200b44058abe7e2963f8f709c
erpi <erpi@users.noreply.github.com>
2019-08-22 11:55:06 +0200

    nieuwe locatie interclubs

--- _scripts/components/home/openstreetmap.js
+++ _scripts/components/home/openstreetmap.js
@@ -9,6 +9,17 @@ function openstreetmap() {
 	       ).addTo(kkmap);
     
     L.marker([50.7466, 4.2576]).addTo(kkmap);
+
+    var nimap = L.map('oudKasteel',
+		      {scrollWheelZoom: false,
+		       dragging: false}
+		     ).setView([50.71942, 4.2568], 16);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		{attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}
+	       ).addTo(nimap);
+    
+    L.marker([50.71948, 4.2568]).addTo(nimap);
 }
 
 $(document).ready(function(){
